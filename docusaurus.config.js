f416b2ec4f43ff17ce8df5393527bd384bc1cf7b
mdecimus <mauro@stalw.art>
2023-08-07 18:14:44 +0200

    Added search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -166,6 +166,35 @@ const config = {
         darkTheme: darkCodeTheme,
         additionalLanguages: ['toml'],
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'CMWSXETHDL',
+
+        // Public API key: it is safe to commit it
+        apiKey: '7559c539ade0f66d36bf423da6c71e5c',
+
+        indexName: 'stalw',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        //externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        /*replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },*/
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        //... other Algolia params
+      },
     }),
 };
 
